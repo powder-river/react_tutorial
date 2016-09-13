5ee43e8bc369315d450c161f4cf319209f4530c0
powder-river <dadkisso@live.com>
2016-09-12 20:34:05 -0400

    ajax call to create item

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,8 +1,22 @@
 var Body = React.createClass({
+  getInitialState(){
+    return { items: [] }
+  },
+
+  handleSubmit(item){
+    var newState = this.state.items.concat(item);
+    this.setState({items: newState})
+  },
+
+  componentDidMount(){
+    $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
+  },
+
   render(){
     return(
       <div>
-        <AllItems/>
+        <NewItem handleSubmit={this.handleSubmit}/>
+        <AllItems items={this.state.items}/>
       </div>
     )
   }
